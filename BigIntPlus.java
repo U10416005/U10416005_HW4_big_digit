45169656b3dea4cd46531acc42c373d1cdf02108
U10416005 <r3r346@gmail.com>
2016-04-03 14:49:38 +0800

    Comment
    Write comments

--- BigIntPlus.java
+++ BigIntPlus.java
@@ -1,13 +1,17 @@
 //10416005ªL«Ø¦t
+
+//import ArrayList to save numbers
 import java.util.ArrayList;
 
 public class BigIntPlus{
+	//Data field
 	private ArrayList<Integer> sum = new ArrayList();
 	private ArrayList<Integer> number1 = new ArrayList();
 	private ArrayList<Integer> number2 = new ArrayList();
 	private String first;
 	private String second;
 	
+	//Constructor
 	public BigIntPlus(String first,String second){
 		this.first = first;
 		this.second = second;
@@ -16,6 +20,7 @@ public class BigIntPlus{
 		checkSize();
 		sumPlus();
 	}
+	//methods to save the number in ArrayList by string
 	public void firstToString(){
 		for(int i = first.length()-1;i>=0;i--){
 			number1.add(Integer.valueOf(String.valueOf(first.charAt(i))));
@@ -26,6 +31,7 @@ public class BigIntPlus{
 			number2.add(Integer.valueOf(String.valueOf(second.charAt(i))));
 		}
 	}
+	//method to check two Arraylist to respond the length
 	public void checkSize(){
 		if(first.length()>second.length()){
 			for(int i = 0;i<(first.length()-second.length());i++){
@@ -38,10 +44,12 @@ public class BigIntPlus{
 			}
 		}
     }
+	//method to plus two Arraylist
 	public void sumPlus(){
 		for(int i = 0;i<number1.size();i++){
 			sum.add(number1.get(i)+number2.get(i));
 		}
+		//if the same place plus over 10,add 1 to next place
 		for(int i = 0;i<sum.size()-1;i++){
 			if(sum.get(i)>=10){
 				sum.set(i+1,sum.get(i+1)+1);
@@ -49,6 +57,7 @@ public class BigIntPlus{
 			}
 		}
 	}
+	//method to print sum result
 	public void printSum(){
 		for(int i = sum.size()-1;i>=0;i--){
 			System.out.print(sum.get(i));
