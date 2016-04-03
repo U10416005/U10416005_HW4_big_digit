bf28ab16e7d5a26572099589efd1203e28072171
U10416005 <r3r346@gmail.com>
2016-04-03 14:49:53 +0800

    Comment
    Write comments

--- TestBigIntPlus.java
+++ TestBigIntPlus.java
@@ -1,12 +1,16 @@
 //U10416005ªL«Ø¦t
+
+//import scanner to enter the number
 import java.util.Scanner;
 
 public class TestBigIntPlus{
 	public static void main(String[] args){
+		//create two scanners
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter two integer to plus");
 		String first = input.next();
 		String second = input.next();
+		//invoke BigIntPlus to caculate
 		BigIntPlus og = new BigIntPlus(first,second);
 		System.out.print("The sum is");
 		og.printSum();
